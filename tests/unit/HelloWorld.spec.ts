299c7f015adac35d83469ccd6b755933bc0daf70
César Alberca <cesalberca@gmail.com>
2018-07-13 11:58:47 +0200

    First commit

--- tests/unit/HelloWorld.spec.ts
+++ tests/unit/HelloWorld.spec.ts
@@ -1,12 +1,16 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
+import VueRouter from "vue-router";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const localVue = createLocalVue();
+    localVue.use(VueRouter);
+    const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+      propsData: { msg },
+      localVue
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
